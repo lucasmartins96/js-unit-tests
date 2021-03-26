5849ad6fb0fdc0e3600b2463aa9cbf3d8befc66b
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-03-26 17:02:06 -0300

    Requisito 4 - 4º teste OK

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -32,7 +32,10 @@ describe('#circle', () => {
     assert.deepStrictEqual(circleEntries.length, 3);
 
     assert.deepStrictEqual(circle(), undefined);
-    // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+
+    const circumferenceRadius2 = circle(2).circumference;
+    assert.deepStrictEqual(circumferenceRadius2, 12.56);
+
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
   });
