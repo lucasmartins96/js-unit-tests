21d452a3484f3d855b33c15319307f888ca97549
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-03-26 18:39:57 -0300

    Requisito 4 - 6º teste OK

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -27,18 +27,18 @@ describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
     // ESCREVA SEUS TESTES ABAIXO:
     assert.ok(typeof circle(1) === 'object');
-
     const circleEntries = Object.entries(circle(1));
     assert.deepStrictEqual(circleEntries.length, 3);
-
     assert.deepStrictEqual(circle(), undefined);
-
     const circumferenceRadius2 = circle(2).circumference;
     assert.deepStrictEqual(circumferenceRadius2, 12.56);
 
     const areaRadius3 = circle(3).area;
     const parseAreaRadius3 = parseFloat(areaRadius3.toPrecision(4));
     assert.deepStrictEqual(parseAreaRadius3, 28.26);
-    // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+
+    const circleRadius3 = circle(3);
+    circleRadius3.area = parseFloat(circleRadius3.area.toPrecision(4));
+    assert.deepStrictEqual(circleRadius3, { radius: 3, area: 28.26, circumference: 18.84 });
   });
 });
