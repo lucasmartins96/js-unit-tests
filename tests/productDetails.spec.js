7b2c8ad52ae7216fb17145a4254883d64e5e9ae7
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-03-26 22:10:45 -0300

    Requisito 6 - 3º teste OK

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -39,6 +39,8 @@ describe('#productDetails', () => {
     // Teste que o array retornado pela função contém dois itens dentro.
     assert.strictEqual(prodDet.length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.ok(typeof prodDet[0] === 'object');
+    assert.ok(typeof prodDet[1] === 'object');
     // Teste que os dois objetos são diferentes entre si.
     // (Difícil) Teste que os dois productIds terminam com 123.
   });
